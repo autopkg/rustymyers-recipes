f2e3faa74c7b570e16c3510b0279dc8cf7d213ff
Rusty Myers <rustymyers@gmail.com>
2018-10-31 10:07:53 -0400

    Adding Audacity download and FossHubURLProvider to get download URL

---
+++ SharedProcessors/FossHubURLProvider.py
@@ -0,0 +1,129 @@
+#!/usr/bin/python
+#
+# Copyright 2018 The Pennsylvania State University.
+#
+# Updated by Rusty Myers (rzm102@psu.edu)
+
+
+import os
+import sys
+import subprocess
+import re
+import urllib2
+import json
+
+from autopkglib import Processor, ProcessorError
+
+
+__all__ = ["FossHubURLProvider"]
+
+
+class FossHubURLProvider(Processor):
+    '''Provides download URL for specific fosshub installer from projects json'''
+    
+    description = "Provides download URL for Box Drive."
+    input_variables = {
+        'projects_url': {
+            "required": False,
+            "description": "URL for Fosshub projects json",            
+        },
+        'app_name': {
+            "required": True,
+            "description": "Name of Fosshub app",
+        },
+        'app_type': {
+            "required": False,
+            "description": "Type of installer prefered, defaults to dmg",
+        },
+    }
+    output_variables = {
+        "url": {
+            "description": "The url for the Box Drive download."
+        },
+        "version": {
+                "description": "The version reported from the json for requested download."
+        },
+        "file_sha1": {
+                "description": "The sha1 of the file download found."
+        },
+        "file_sha256": {
+                "description": "The sha256 of the file download found."
+        }
+    }
+
+    __doc__ = description
+
+    def get_json(self,url):
+        hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'}
+        request = urllib2.Request(url, headers=hdr)
+        try:
+            f = urllib2.urlopen(request)
+            raw_json = f.read()
+            # print raw_json
+            f.close()
+        except:
+            raise ProcessorError('Could not retrieve projects URL "%s"' % url)
+
+        return json.loads(raw_json)
+        
+    def unique_types(self,json):
+        file_types=[]
+        # Return list of unique file types
+        for project in json['projects']:
+            for projectfile in project['files']:
+                file_types.append(projectfile['type'])
+        myset = set(file_types)
+        for filetype in list(myset):
+            print filetype
+        
+            
+    def main(self):
+        projects_url = self.env.get('projects_url', "https://university.fosshub.com/projects.json")
+        app_name = self.env.get('app_name')
+        app_type = self.env.get('app_type', "macOS DMG")
+        verbosity = self.env.get('verbose', 0)
+        
+        
+        if verbosity > 1:
+            self.output("Checking json for: '{0}' & '{1}' from {2}".format(app_name, app_type, projects_url))
+            
+        try:
+            json_results = self.get_json(projects_url)
+        except:
+            raise
+
+        if verbosity > 2:
+            # Print unique file types
+            self.unique_types(json_results)
+
+        # Print last updated time of json
+        last_update = json_results['lastUpdatedAt']
+        if verbosity > 1:
+            self.output("Projects JSON last updated: {0}".format(last_update))
+
+        # Check each project name to find app_name
+        for project in json_results['projects']:
+            # If we find a project that matches
+            if project['name'] == app_name:
+                # print project['name']
+                # print project
+                # Check each file in the matched project
+                for projectfile in project['files']:
+                    # print projectfile
+                    # print projectfile['name']
+                    # print projectfile['downloadUrl']
+                    # print projectfile['version']
+                    # print projectfile['hashes']
+                    # print projectfile['type']
+                    # If we match the app type in the project file
+                    if projectfile['type'] == app_type:
+                        # Return match if found
+                        self.env['url'] = projectfile['downloadUrl']
+                        self.env['version'] = projectfile['version']
+                        self.env['file_sha1'] = projectfile['hashes']['sha1']
+                        self.env['file_sha256'] = projectfile['hashes']['sha256']
+                        continue
+
+if __name__ == '__main__':
+    processor = FossHubURLProvider()
+    processor.execute_shell()
