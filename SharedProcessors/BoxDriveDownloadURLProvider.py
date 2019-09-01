2f9f0ab2a955ab8d5f21885faf4c9004f2fb22ca
Elliot Jordan <elliot@elliotjordan.com>
2019-08-31 20:23:52 -0700

    Standardize indentation, adjust imports

--- SharedProcessors/BoxDriveDownloadURLProvider.py
+++ SharedProcessors/BoxDriveDownloadURLProvider.py
@@ -8,27 +8,23 @@
 
 
 from __future__ import absolute_import
-from __future__ import print_function
-import os
-import sys
-import subprocess
-import re
 import json
+import subprocess
+import urllib2
 
 from autopkglib import Processor, ProcessorError
-
 
 __all__ = ["BoxDriveDownloadURLProvider"]
 
 
 class BoxDriveDownloadURLProvider(Processor):
     '''Provides download URL for specific box drive installer from update json'''
-    
+
     description = "Provides download URL for Box Drive."
     input_variables = {
         'update_url': {
             "required": False,
-            "description": "URL for Box json",            
+            "description": "URL for Box json",
         },
         'os_type': {
             "required": True,
@@ -42,7 +38,7 @@ class BoxDriveDownloadURLProvider(Processor):
             "required": False,
             "description": "URL type to check for: rollout-url, download-url. Defaults to download-url",
         },
-            "CURL_PATH": {
+        "CURL_PATH": {
             "required": False,
             "default": "/usr/bin/curl",
             "description": "Path to curl binary. Defaults to /usr/bin/curl.",
@@ -53,22 +49,22 @@ class BoxDriveDownloadURLProvider(Processor):
             "description": "The url for the Box Drive download."
         },
         "version": {
-                "description": "The version reported from the json for requested download."
+            "description": "The version reported from the json for requested download."
         }
     }
 
     __doc__ = description
 
-    def get_json(self,url):
-		try:
-			f = urllib2.urlopen(url)
-			raw_json = f.read()
-			f.close()
-		except:
-			raise ProcessorError('Could not retrieve URL: "%s"' % url)
+    def get_json(self, url):
+        try:
+            f = urllib2.urlopen(url)
+            raw_json = f.read()
+            f.close()
+        except:
+            raise ProcessorError('Could not retrieve URL: "%s"' % url)
 
-		return json.loads(raw_json)
-        
+        return json.loads(raw_json)
+
     def fetch_content(self, url, headers=None):
         """Returns content retrieved by curl, given a url and an optional
         dictionary of header-name/value mappings. Logic here borrowed from
@@ -90,16 +86,16 @@ class BoxDriveDownloadURLProvider(Processor):
 
     def main(self):
 
-        default_box_json_url="https://cdn07.boxcdn.net/Autoupdate.json"
+        default_box_json_url = "https://cdn07.boxcdn.net/Autoupdate.json"
         os_type = self.env.get('os_type')
         release = self.env.get('release')
         update_url = self.env.get('update_url', default_box_json_url)
         url_type = self.env.get('url_type', 'download-url')
         verbosity = self.env.get('verbose', 0)
-        
+
         if verbosity > 1:
             self.output("Checking json for: {0}, {1}, {2} from {3}".format(os_type, release, url_type, update_url))
-            
+
         json_results = self.fetch_content(update_url)
 
         try:
@@ -107,8 +103,8 @@ class BoxDriveDownloadURLProvider(Processor):
         except:
             self.output("Failed to get %s, checking for download-url" % url_type)
             box_download_url = json_results[os_type][release]['download-url']
-        
-        print(box_download_url)
+
+        self.output(box_download_url)
         try:
             # Get the version associated with the download type
             if "rollout" in url_type:
@@ -117,7 +113,7 @@ class BoxDriveDownloadURLProvider(Processor):
                 box_version = json_results[os_type][release]["version"]
         except:
             box_version = json_results[os_type][release]["version"]
-            
+
 
         self.env['url'] = box_download_url
         self.env['version'] = box_version
